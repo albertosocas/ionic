18236a09dea63c112bbb43348f474dc12a60b5a7
alber <albertitosocas@hotmail.com>
2022-05-18 12:27:13 +0100

    perfil añadido

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NavController} from '@ionic/angular';
+import {AuthenticationService} from '../services/authentication.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  userEmail: string;
+
+  constructor(private navCtrl: NavController,
+              private authService: AuthenticationService) { }
 
   ngOnInit() {
+    this.authService.userDetails().subscribe(res => {
+      console.log('res', res);
+      if (res !== null) {
+        this.userEmail = res.email;
+      } else {
+        this.navCtrl.navigateBack('');
+      }
+    }, err => {
+      console.log('err', err);
+    });
   }
 
+  logout() {
+    this.authService.logoutUser()
+      .then(res => {
+        console.log(res);
+        this.navCtrl.navigateBack('');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 }
+
